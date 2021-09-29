379f7a8e9f8feacf4124f118b21003577828f411
Michael Moriarty <mmoriarty316@gmail.com>
2021-09-28 19:27:50 -0700

    wrote Blockchain class

--- js-blockchain/main.js
+++ js-blockchain/main.js
@@ -19,3 +19,35 @@ class Block {
   }
 };
 
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesis()];
+  }
+
+  createGenesis() {
+    return new Block(0, "09/28/21", "Genesis block", "0");
+  }
+
+  latestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBock.previousHash = this.latestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  checkValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) return false;
+      if (currentBlock.previousHash !== previousBlock.hash) return false;
+
+      return true;
+    }
+  }
+}
+
